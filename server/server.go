8863f8863b462260792090ba2a279e9343a1945a
Anup <aglawe.anup22@gmail.com>
2020-08-11 10:42:53 +0530

    🔥 enabled cors

--- server/server.go
+++ server/server.go
@@ -26,10 +26,11 @@ func main() {
 
 	opts := []graphql.SchemaOpt{graphql.UseFieldResolvers()}
 	schema := graphql.MustParseSchema(*schemas.NewSchema(), &resolvers.Resolvers{DB: db}, opts...)
+	h := handler.Authenticate(&handler.GraphQL{Schema: schema})
 
 	mux := http.NewServeMux()
 	mux.Handle("/", handler.GraphiQL{})
-	mux.Handle("/query", handler.Authenticate(&handler.GraphQL{Schema: schema}))
+	mux.Handle("/query", disableCors(h))
 
 	s := &http.Server{
 		Addr:    ":8080",
@@ -41,4 +42,22 @@ func main() {
 		panic(err)
 	}
 
+}
+
+func disableCors(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Authorization, Content-Type, Content-Length, Accept-Encoding")
+
+		if r.Method == "OPTIONS" {
+			w.Header().Set("Access-Control-Max-Age", "86400")
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		h.ServeHTTP(w, r)
+	})
 }
