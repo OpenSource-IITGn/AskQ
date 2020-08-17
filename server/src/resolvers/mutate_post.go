cb763d7cdb7e93a7a4027bd94067656f68981217
bridgesign <rohan@DESKTOP-M1S2CKJ.localdomain>
2020-08-17 07:04:46 +0530

    Postype set correction

--- server/src/resolvers/mutate_post.go
+++ server/src/resolvers/mutate_post.go
@@ -37,6 +37,7 @@ func (r *Resolvers) CreatePost(ctx context.Context ,args CreatePostArgs) (*Query
 	postdet := model.PostDetails{}
 	postdet.Post = post
 	post.User = *(profile.User.U)
+	post.PostType = args.PostType
 	
 	// Check if answer and validate Question ID
 	if args.PostType==1 {
