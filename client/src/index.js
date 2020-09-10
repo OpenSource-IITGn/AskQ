b80b799ab238730299a3a945fd418d0b8c652aff
anup-a <aglawe.anup22@gmail.com>
2020-09-10 23:03:40 +0530

    ⭕ removed unncessary consoles

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import ReactDOM from 'react-dom';
-import * as serviceWorker from './serviceWorker';
+import ReactDOM from "react-dom";
+import * as serviceWorker from "./serviceWorker";
 
-import 'rsuite/dist/styles/rsuite-default.css';
+import "rsuite/dist/styles/rsuite-default.css";
 
-import App from './App';
+import App from "./App";
 
-import './index.css';
+import "./index.css";
 
-// console.log("apolloClient.link", apolloClient.link)
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
