07f9447cf4144581d056a65a8549665111c61e9c
anup-a <aglawe.anup22@gmail.com>
2020-09-11 12:03:32 +0530

    🍕 logged out nav state changed

--- client/src/Components/Navbar/navbar.js
+++ client/src/Components/Navbar/navbar.js
@@ -1,40 +1,88 @@
-import { Navbar, Nav, Icon, InputGroup, Input } from 'rsuite';
-import React from 'react'
-import { NavLink, Link } from 'react-router-dom';
-import { useLogout } from '../../hooks/auth';
+import React, { useContext, useState } from "react";
 
-import './navbar.css'
+import { Navbar, Nav, Icon, InputGroup, Input } from "rsuite";
+import { NavLink, Link } from "react-router-dom";
+import { useLogout } from "../../hooks/auth";
+
+import "./navbar.css";
+import { UserContext } from "../../Contexts/UserContext";
+import { sucessAlert } from "../errorHandler";
 
 function CustomNavbar(props) {
+  var loggedOut = false;
+  if (props.history.location.state) {
+    const state = props.history.location.state;
+    loggedOut = state.logout;
+  }
 
-    const logout = useLogout()
+  const { authenticated, user } = useContext(UserContext);
+  const isAuthenticated = authenticated && user ? true : false;
+  const [authState, setAuthState] = useState(!loggedOut && isAuthenticated);
 
-    const handleLogout = async () => {
+  const logout = useLogout();
 
-        await logout();
-        alert('Successfuly Logged out')
-        props.history.push('/')
-    }
+  const handleLogout = async () => {
+    await logout();
+    props.history.push({ pathname: "/", state: { logout: true } });
 
-    return (
-        <Navbar appearance="subtle">
+    sucessAlert("Logged out successfuly");
+    setAuthState(false);
+  };
 
-            <Navbar.Body className="navbar">
-                <Nav className="navbar-nav" pullRight>
-                    <Nav.Item className="navbar-item" componentClass={Link} style={{ color: "#fff", height: "40px" }} to="/" >About</Nav.Item>
-                    <Nav.Item className="navbar-item special-btn" style={{ color: "#fff", height: "40px" }} componentClass={Link} to="/login">Login</Nav.Item>
-                    <Nav.Item className="navbar-item" componentClass={Link} style={{ color: "#fff", height: "40px" }} to="/signup">Signup</Nav.Item>
-                </Nav >
+  const unAuthNavItems = (
+    <>
+      <Nav.Item
+        className="navbar-item special-btn"
+        style={{ color: "#fff", height: "40px" }}
+        componentClass={Link}
+        to="/login"
+      >
+        Login
+      </Nav.Item>
+      <Nav.Item
+        className="navbar-item"
+        componentClass={Link}
+        style={{ color: "#fff", height: "40px" }}
+        to="/signup"
+      >
+        Signup
+      </Nav.Item>
+    </>
+  );
 
-                {/* <Nav className="navbar-nav" pullRight>
+  const authNavItems = (
+    <Nav.Item
+      className="navbar-item"
+      componentClass={Link}
+      style={{ color: "#fff", height: "40px" }}
+      onClick={handleLogout}
+    >
+      Logout
+    </Nav.Item>
+  );
+
+  return (
+    <Navbar appearance="subtle">
+      <Navbar.Body className="navbar">
+        <Nav className="navbar-nav" pullRight>
+          <Nav.Item
+            className="navbar-item"
+            componentClass={Link}
+            style={{ color: "#fff", height: "40px" }}
+            to="/"
+          >
+            About
+          </Nav.Item>
+          {authState ? authNavItems : unAuthNavItems}
+        </Nav>
+
+        {/* <Nav className="navbar-nav" pullRight>
                     <Nav.Item onClick={handleLogout}>Log Out</Nav.Item>
                     <Nav.Item icon={<Icon icon="cog" />} componentClass={Link} to="/dashboard">Dashboard</Nav.Item>
                 </Nav> */}
-            </Navbar.Body >
-        </Navbar >
-
-    );
-
+      </Navbar.Body>
+    </Navbar>
+  );
 }
 
 export default CustomNavbar;
