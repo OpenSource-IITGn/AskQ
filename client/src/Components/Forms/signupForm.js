98c7aca9704349fab1b4cef6f58106c3a502a634
anup-a <aglawe.anup22@gmail.com>
2020-09-11 14:43:28 +0530

    🐛 signup username bug fix

--- client/src/Components/Forms/signupForm.js
+++ client/src/Components/Forms/signupForm.js
@@ -1,81 +1,106 @@
-import React, { useState } from 'react'
-import { Form, FormGroup, FormControl, ControlLabel, HelpBlock, Button, ButtonToolbar } from 'rsuite';
-import { useSignupMutation } from '../../GraphQL/Mutations/signupMutation'
+import React, { useState } from "react";
+import {
+  Form,
+  FormGroup,
+  FormControl,
+  ControlLabel,
+  HelpBlock,
+  Button,
+  ButtonToolbar,
+} from "rsuite";
+import { useSignupMutation } from "../../GraphQL/Mutations/signupMutation";
 
-import '../../styles/global.css';
-import { TextField, model } from '../formValidation';
-import { invalidError, alreadySignedError, unknownError } from '../errorHandler';
-import { isEmpty } from '../../utils';
-
+import "../../styles/global.css";
+import { TextField, model } from "../formValidation";
+import {
+  invalidError,
+  alreadySignedError,
+  unknownError,
+  sucessAlert,
+} from "../errorHandler";
+import { isEmpty } from "../../utils";
 
 const SignupForm = (props, { loading }) => {
   const [signupMutation, signupMutationResults] = useSignupMutation();
 
-  // const onSubmit = (values) => loginMutation(values.username, values.password);
-  const [username, setUsername] = useState('help');
-  const [email, setEmail] = useState('azleblade@gmail.com');
-  const [password, setPassword] = useState('azleblade');
-  const [verifyPassword, setVerifyPassword] = useState('azleblade');
+  const [username, setUsername] = useState("help");
+  const [email, setEmail] = useState("azleblade@gmail.com");
+  const [password, setPassword] = useState("azleblade");
+  const [verifyPassword, setVerifyPassword] = useState("azleblade");
   const [formError, setFormError] = useState();
-
 
   const handleSubmit = async () => {
     if (isEmpty(formError)) {
-      invalidError(formError.name)
+      invalidError(formError.name);
       return;
     }
     try {
-      const data = await signupMutation(username, email, password)
-      const response = data.data.signUp
+      const data = await signupMutation(username, email, password);
+      const response = data.data.signUp;
 
       if (response.ok === 104) {
-        alreadySignedError()
-        props.history.push('/login')
+        alreadySignedError();
+        props.history.push("/login");
+      } else if (response.ok === 103) {
+        alreadySignedError();
+      } else if (response.ok === 100) {
+        sucessAlert("Registered successfuly");
+        props.history.push("/login");
       }
-
     } catch (e) {
-      unknownError('Network or server Error')
+      unknownError("Network or server Error");
     }
-
-  }
+  };
 
   const handleChange = (value, evt) => {
-    if (evt.target.name === 'email') {
-      setEmail(value)
-    } else if (evt.target.name === 'password') {
-      setPassword(value)
+    if (evt.target.name === "email") {
+      setEmail(value);
+    } else if (evt.target.name === "password") {
+      setPassword(value);
     } else if (evt.target.name === "username") {
-      setUsername(value)
+      setUsername(value);
     } else if (evt.target.name === "verifyPassword") {
-      setVerifyPassword(value)
+      setVerifyPassword(value);
     }
-  }
+  };
 
   return (
     <Form
       onSubmit={handleSubmit}
       model={model}
-      onCheck={formError => {
+      onCheck={(formError) => {
         setFormError({ formError });
       }}
       formValue={{
         username,
         email,
         password,
-        verifyPassword
+        verifyPassword,
       }}
       fluid
     >
-      <TextField name="name" label="Username" onChange={handleChange} />
+      <TextField name="username" label="Username" onChange={handleChange} />
       <TextField name="email" label="Email" onChange={handleChange} />
-      <TextField name="password" label="Password" type="password" onChange={handleChange} />
-      <TextField name="verifyPassword" label="Verify password" type="password" onChange={handleChange} />
+      <TextField
+        name="password"
+        label="Password"
+        type="password"
+        onChange={handleChange}
+      />
+      <TextField
+        name="verifyPassword"
+        label="Verify password"
+        type="password"
+        onChange={handleChange}
+      />
       <ButtonToolbar>
-        <Button type="submit" appearance="primary">Login</Button>
+        <Button type="submit" appearance="primary">
+          Signup
+        </Button>
       </ButtonToolbar>
       {/* </FormGroup> */}
-    </Form >
-  )
+    </Form>
+  );
 };
 
 export default SignupForm;
